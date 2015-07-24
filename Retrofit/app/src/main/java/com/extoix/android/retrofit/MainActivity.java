f3209b67bdec41cbbbbe870b65eb84b5d4363fd7
John Kuk <extoix@users.noreply.github.com>
2015-07-24 13:32:30 -0500

    Simple Retrofit example using GitHub

--- Retrofit/app/src/main/java/com/extoix/android/retrofit/MainActivity.java
+++ Retrofit/app/src/main/java/com/extoix/android/retrofit/MainActivity.java
@@ -1,17 +1,72 @@
 package com.extoix.android.retrofit;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
 
 public class MainActivity extends ActionBarActivity {
+    public class Contributor {
+
+        private String login;
+        private int contributions;
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public int getContributions() {
+            return contributions;
+        }
+
+        public void setContributions(int contributions) {
+            this.contributions = contributions;
+        }
+
+    }
+
+    public interface GitHub {
+        @GET("/repos/{owner}/{repo}/contributors")
+        void contributors(@Path("owner") String owner, @Path("repo") String repo, Callback<List<Contributor>> callback);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.github.com").build();
+        GitHub github = restAdapter.create(GitHub.class);
+
+        github.contributors("extoix", "android-examples", new Callback<List<Contributor>>() {
+            @Override
+            public void success(List<Contributor> contributors, Response response) {
+                Contributor contributor = contributors.get(0);
+
+                String login = contributor.getLogin();
+                int contributions = contributor.getContributions();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
     }
 
     @Override
